4e408f6e2bd27a7756ed62b3eee48ca950fddb7e
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-10 16:54:30 -0500

    mi primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+window.addEventListener('load',function(e) {
+ for(var i=1; i<13 ; i++){
+   var div=document.createElement("div");
+   var fig=document.createElement("figure");
+   var img1=document.createElement("img");
+   img1.setAttribute("src","assets/img/img-"+i+".jpg");
+   img1.setAttribute("alt","imagen-"+i);
+   var caption=document.createElement("figcaption");
+   caption.innerHTML="nombre del proyecto";
+   fig.appendChild(img1);
+   fig.appendChild(caption);
+   div.appendChild(fig);
+   document.getElementById("insertaImg").appendChild(div);
+ }
+});
