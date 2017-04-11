6f59af6d87606342bd75661ce2e83540014e6a69
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-11 11:38:28 -0500

    falta formato modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*window.addEventListener('load',function(e) {
+window.addEventListener('load',function(e) {
  for(var i=1; i<13 ; i++){
    var div=document.createElement("div");
    var fig=document.createElement("figure");
@@ -12,25 +12,23 @@
    div.appendChild(fig);
    document.getElementById("insertaImg").appendChild(div);
  }
-});*/
+});
 
-// Get the modal
+var miSeccion=document.getElementById("insertaImg");
 var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+var miTexto=document.getElementById("textoImagen");
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
+
+
+
+miSeccion.onclick = function(e){
     modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+    modalImg.src = e.target.src;
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
