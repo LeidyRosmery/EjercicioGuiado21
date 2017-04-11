1aff44072239d20b646f97fa8f12a44338d048ba
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-11 01:04:37 -0500

    pruebas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('load',function(e) {
+/*window.addEventListener('load',function(e) {
  for(var i=1; i<13 ; i++){
    var div=document.createElement("div");
    var fig=document.createElement("figure");
@@ -12,4 +12,25 @@ window.addEventListener('load',function(e) {
    div.appendChild(fig);
    document.getElementById("insertaImg").appendChild(div);
  }
-});
+});*/
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
