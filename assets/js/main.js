d58597150484d9759d509601da3d298cc98472b9
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-11 15:34:22 -0500

    listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,33 @@
-window.addEventListener('load',function(e) {
- for(var i=1; i<13 ; i++){
-   var div=document.createElement("div");
-   var fig=document.createElement("figure");
-   var img1=document.createElement("img");
-   img1.setAttribute("src","assets/img/img-"+i+".jpg");
-   img1.setAttribute("alt","imagen-"+i);
-   var caption=document.createElement("figcaption");
-   caption.innerHTML="nombre del proyecto";
-   fig.appendChild(img1);
-   fig.appendChild(caption);
-   div.appendChild(fig);
-   document.getElementById("insertaImg").appendChild(div);
- }
+window.addEventListener('load', function(e) {
+    for (var i = 1; i < 13; i++) {
+        var div = document.createElement("div");
+        //div.setAttribute("class","modal");
+        var fig = document.createElement("figure");
+        var img1 = document.createElement("img");
+        img1.setAttribute("src", "assets/img/img-" + i + ".jpg");
+        img1.setAttribute("alt", "imagen-" + i);
+        var caption = document.createElement("figcaption");
+        caption.innerHTML = "nombre del proyecto";
+        fig.appendChild(img1);
+        fig.appendChild(caption);
+        div.appendChild(fig);
+        document.getElementById("insertaImg").appendChild(div);
+        img1.addEventListener('click', function() {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+        });
+    }
 });
 
-var miSeccion=document.getElementById("insertaImg");
+var miSeccion = document.getElementById("insertaImg");
 var modal = document.getElementById('myModal');
-var miTexto=document.getElementById("textoImagen");
+var miTexto = document.getElementById("textoImagen");
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 
-
-miSeccion.onclick = function(e){
-    modal.style.display = "block";
-    modalImg.src = e.target.src;
-}
-
+// img.addEventListener('click',mostrarModal());
 
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
